6b7bdcfc7fbc8546db76c2bd23e27cf0fb267c9d
ha963331 <ha963331@users.noreply.github.com>
2020-12-29 15:30:48 +0800

    first commit

---
+++ mblog/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from mainsite.views import homepage, lotto, showpost, mychart, chart
+
+
+urlpatterns = [
+    path('post/<str:slug>/', showpost),
+    path('admin/', admin.site.urls),
+    path('lotto/', lotto),
+    path('mychart/', mychart),
+    path('mychart/<int:bid>/', mychart),
+    path('chartbydate/<int:year>/<int:month>/', chart),
+    path('chartbydate/<int:year>/', chart),
+    path('', homepage),
+    
+]
